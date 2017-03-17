d0908c20e6e2430ba6cb9a76aba49b803c476021
Enricespel <espelenric@gmail.com>
2017-03-17 11:33:08 +0100

    Final changes to secure that week 3 works.
    Final changes to analyse, goalController, tracker, to make sure they
    work.

--- public/javascripts/goalController.js
+++ public/javascripts/goalController.js
@@ -1,45 +1,29 @@
 angular.module('goalController', [])
 .controller('goalCtrl', function($scope, $http, Goals) {
   Goals.get().success(function(data) {
-<<<<<<< HEAD
-    for (var currentIndex=0; currentIndex<data.length; currentIndex=currentIndex+1) {
-    var currentElement = data[currentIndex];
-      currentElement.percentatge = (currentElement.ok/(currentElement.ok+currentElement.ko))*100;
-    }
-    $scope.goals = data;
-=======
-    $scope.loadDay(new Date(), data);
->>>>>>> profjesus/master
-  });
-  $scope.loadDay = function(date, goals) {
-    Goals.getDay(date).success(function(day) {
+    Goals.getDay().success(function(day) {
       if (!day) {
-        var originalDate = new Date(date);
-        day = {day: originalDate};
-      } else { // Convert to javascript date in order to avoid datepicker problems
+        var today = new Date();
+        day = {day: today};
+      } else {
         day.day = new Date(day.day);
       }
-      $scope.goals = updateGoals(goals, day);
-      $scope.day = day.day;
-    });
-  };
-  updateGoals = function (goals, day) {
-    for (var i=0; i<goals.length; i++) {
-      var currentElement = goals[i];
-      currentElement.percentage = (currentElement.ok/(currentElement.ok+currentElement.ko))*100;
-      if (day.goals) {
-        for (var j=0; j<day.goals.length; j++) {
-          if (day.goals[j]._id == currentElement._id) {
-            currentElement.result = day.goals[j].result;
-            break;
+      for (var currentIndex=0; currentIndex<data.length; currentIndex=currentIndex+1) {
+        var currentElement = data[currentIndex];
+        currentElement.percentage = (currentElement.ok/(currentElement.ok+currentElement.ko))*100;
+        if (day.goals) {
+          for (var j=0; j<day.goals.length; j++) {
+            if (day.goals[j]._id == currentElement._id) {
+              currentElement.result = day.goals[j].result;
+              break;
+            }
           }
         }
-      } else {
-        currentElement.result = 0;
       }
-    }
-    return(goals);
-  };
+      $scope.goals = data;
+      $scope.day = day.day;
+    });
+  });
   $scope.addGoal = function() {
     if(!$scope.goalName || $scope.goalName === '') { return; }
     var goalObject = {
@@ -48,13 +32,8 @@ angular.module('goalController', [])
       ko: 0
     };
     Goals.create(goalObject).success(function(data) {
-<<<<<<< HEAD
-      goalObject._id=data._id;
-      $scope.goals.push(goalObject);
-=======
       data.percentage = 0;
-      $scope.goals.push(data);
->>>>>>> profjesus/master
+      $scope.goals.push(goalObject);
       $scope.goalName = '';
     });
   };
@@ -62,20 +41,16 @@ angular.module('goalController', [])
     Goals.delete(goal._id).success(function(data) {
       var index = $scope.goals.indexOf(goal);
       $scope.goals.splice(index, 1);
-$scope.addOk = function(goal) {
-  Goals.addOk(goal._id).success(function(data) {
-});
-};
-$scope.addKo = function(goal) {
-  Goals.addKo(goal._id).success(function(data) {
     });
   };
   $scope.addOk = function(goal) {
     Goals.addOk(goal._id, $scope.day).success(function(data) {
+      goal.result=1;
     });
   };
   $scope.addKo = function(goal) {
     Goals.addKo(goal._id, $scope.day).success(function(data) {
+      goal.result=-1;
     });
   };
 });
